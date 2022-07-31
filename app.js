ee603ebeb0b0f5fc0b549849c1c024109113e09f
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-07-31 17:11:19 +0200

    Connected to mongo db

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connected');
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -8,6 +17,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/campground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!' });
+    await camp.save();
+    res.send(camp);
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
