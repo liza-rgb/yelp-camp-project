fdef12318bb921a06fa11ec7dfc9e56710823135
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-08-10 14:19:11 +0200

    Served static assets

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
-const wrapAsync = require('./utils/wrapAsync');
 const ExpressError = require('./utils/ExpressError'); 
 const methodOverride = require('method-override');
 
@@ -23,6 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
